f30e4abfa8e849a7f586a41a4ceb41e2ff9de938
joshrmcdaniel <80354972+joshrmcdaniel@users.noreply.github.com>
2023-10-11 09:49:20 -0500

    pagination

--- overseerr.py
+++ overseerr.py
@@ -1,5 +1,3 @@
-import aiohttp
-from typing import TypedDict
 import os
 import sys
 import discord
@@ -8,7 +6,7 @@ from discord.commands import slash_command, Option
 from discord.commands.context import ApplicationContext
 from discord.ui.item import Item
 import loguru
-from typing import List, Self, Dict
+from typing import Self, Dict
 import networking
 import shared
 import overseerr_types as OverseerrTypes
@@ -19,19 +17,15 @@ logger.add(sys.stderr, level="TRACE")
 
 
 class Overseerr(commands.Cog):
-    def __init__(self, bot: commands.Cog):
-        self._url: str = os.environ.get("OVERSEERR_URL")
-        self._token: str = os.environ.get("OVERSEERR_TOKEN")
-        self._discord_id_map: Dict[int, int] = {}
-        pass
+    _url: str = os.environ.get("OVERSEERR_URL")
+    _headers: Dict[str, str] = {
+        "X-Api-Key": os.environ.get("OVERSEERR_TOKEN"),
+        "Content-Type": "application/json",
+    }
+    _discord_id_map: Dict[int, int] = {}
 
-    async def _setup_discord_id_map(self) -> Dict[int, int]:
-        users = (await networking.get(f"{self._url}/user", headers=self._headers))['results']
-        user_ids = map(lambda x: x["id"], users)
-        for i in user_ids:
-            user = await networking.get(f"{self._url}/user/{i}", headers=self._headers)
-            discord_id = user["settings"]["discordId"]
-            self._discord_id_map[int(discord_id)] = i
+    def __init__(self, bot: commands.Cog):
+        self._bot = bot
 
     @slash_command(
         name="search",
@@ -42,58 +36,70 @@ class Overseerr(commands.Cog):
         self,
         ctx: ApplicationContext,
         query: Option(str, "Media to search for.", required=True),
+        page: Option(
+            int, "Media to search for.", min_value=1, required=False, default=1
+        ),
     ):
         """Searches for a movie or tv show"""
-        params = {"query": query, "page": 1}
-        data = await networking.get(
-            f"{self._url}/search", params=params, headers=self._headers
-        )
-        with open("res2.json", "w") as f:
-            import json
-
-            json.dump(data, f)
-
-        if isinstance(data, aiohttp.ClientResponseError):
-            await ctx.respond(f"`Error occured during Overseerr request.`")
-            return
-        data: OverseerrTypes.Search = await self._parse_search(data)
-        if data.total_results == 0:
-            await ctx.respond(f"`No results found.`")
-            return
-        if self._discord_id_map == {}:
-            await self._setup_discord_id_map()
-        view = SearchView(search_results=data, discord_id_map=self._discord_id_map, headers=self._headers, url=self._url)
+        results = await Overseerr.search(query, page)
+        view = SearchView(results=results, search_query=query)
         await view._edit_embed()
         await ctx.respond(embed=view.embed, view=view)
 
-    async def _parse_search(self, data: OverseerrTypes.Search) -> OverseerrTypes.Search:
-        data = await shared.load_json(data, OverseerrTypes.Search)
+    @staticmethod
+    async def search(
+        query, page=1
+    ) -> OverseerrTypes.Search | OverseerrTypes.OverseerrErrorResult:
+        params = {"query": query, "page": page}
+        data = await networking.get(
+            f"{Overseerr._url}/search", params=params, headers=Overseerr._headers
+        )
+        if isinstance(data, OverseerrTypes.OverseerrErrorResult):
+            logger.error("Error occured during Overseerr request: {}", data)
+        else:
+            data = await shared.load_json(data, OverseerrTypes.Search)
         return data
 
-    @property
-    def _headers(self) -> dict:
-        return {"X-Api-Key": self._token, "Content-Type": "application/json"}
+    @staticmethod
+    async def _setup_discord_id_map() -> Dict[int, int]:
+        ids = {}
+        users = (
+            await networking.get(f"{Overseerr._url}/user", headers=Overseerr._headers)
+        )["results"]
+        user_ids = map(lambda x: x["id"], users)
+        for i in user_ids:
+            user = await networking.get(
+                f"{Overseerr._url}/user/{i}", headers=Overseerr._headers
+            )
+            discord_id = user["settings"]["discordId"]
+            ids[int(discord_id)] = i
+        return ids
 
-async def _post_request(*, url: str, headers: Dict[str, str], media_id: int, media_type: str, user_id: int) -> None:
-    logger.debug("Posting request")
-    payload = {
-        "mediaId": media_id,
-        "mediaType": media_type,
-        "profileId": user_id,
-    }
-    logger.trace("Body: {}", payload)
+    @staticmethod
+    async def post_request(*, media_id: int, media_type: str, discord_id: int) -> None:
+        logger.debug("Posting request")
+        if discord_id not in Overseerr._discord_id_map:
+            logger.debug("User ID {} not in map", discord_id)
+            Overseerr._discord_id_map = await Overseerr._setup_discord_id_map()
+        user_id = Overseerr._discord_id_map[discord_id]
+        payload = {
+            "mediaId": media_id,
+            "mediaType": media_type,
+            "profileId": user_id,
+        }
+        logger.trace("Body: {}", payload)
 
-    await networking.post(f"{url}/request", body=payload, headers=headers)
+        await networking.post(
+            f"{Overseerr._url}/request", body=payload, headers=Overseerr._headers
+        )
 
 
 class SearchView(discord.ui.View):
     def __init__(
         self,
+        search_query: str,
+        results: OverseerrTypes.Search,
         *items: Item,
-        search_results: OverseerrTypes.Search,
-        headers: Dict[str, str],
-        discord_id_map: Dict[int, int],
-        url: str,
         timeout: float | None = 180,
         disable_on_timeout: bool = False,
     ) -> Self:
@@ -101,11 +107,9 @@ class SearchView(discord.ui.View):
         self._index: int = 0
         self._poster_base: str = "https://image.tmdb.org/t/p/w342"
         self._backdrop_base: str = "https://image.tmdb.org/t/p/w300"
-        self._results: List[OverseerrTypes.SearchResult] = search_results.results
-        self._result_length: int = search_results.total_results
-        self._discord_id_map: Dict[int, int] = discord_id_map
-        self._url: str = url
-        self._headers: Dict[str, str] = headers
+        self._query: str = search_query
+        self._results: OverseerrTypes.Search = results
+        self._result_length: int = self._results.total_results
         self.embed: discord.Embed = discord.Embed(color=discord.Color.blurple())
         # Disable the left button if we're on the first page
         self.children[0].disabled = True
@@ -115,7 +119,10 @@ class SearchView(discord.ui.View):
         self, button: discord.ui.Button, interaction: discord.Interaction
     ) -> None:
         self._index -= 1
-        button.disabled = self._index <= 0
+        button.disabled = self._results.page == 1 and self._index <= 0
+        if self._index == 0 and self._results.page > self._results.total_pages:
+            self._results = await Overseerr.search(self._query, self._results.page - 1)
+            self._index = 0
         await self._paginate(interaction)
 
     @discord.ui.button(style=discord.ButtonStyle.primary, label=">")
@@ -123,36 +130,40 @@ class SearchView(discord.ui.View):
         self, button: discord.ui.Button, interaction: discord.Interaction
     ) -> None:
         self._index += 1
-        button.disabled = self._index >= self._result_length - 1
+        button.disabled = (
+            self._results.total_pages == self._results.page
+            and self._index * self._results.page >= self._result_length - 1
+        )
+        if self._index == 20 and self._results.page < self._results.total_pages:
+            self._results = await Overseerr.search(self._query, self._results.page + 1)
+            self._index = 0
         await self._paginate(interaction)
 
     @discord.ui.button(style=discord.ButtonStyle.success, label="Request")
     async def button_callback3(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ) -> None:
-        user_id = self._discord_id_map[interaction.user.id]
         button.disabled = True
         self.embed.title = f"Sending requests for {self.embed.title}..."
         await interaction.response.edit_message(embed=self.embed, view=self)
-        await _post_request(
-            url=self._url,
-            headers=self._headers,
-            media_id=self._results[self._index].id,
-            media_type=self._results[self._index].media_type,
-            user_id=user_id
+        await Overseerr.post_request(
+            media_id=self._results.results[self._index].id,
+            media_type=self._results.results[self._index].media_type,
+            discord_id=interaction.user.id,
         )
         button.label = "Request Sent"
-        if self._results[self._index].media_type == "movie":
-            name =  self._results[self._index].title
-        elif self._results[self._index].media_type == "tv":
-            name = self._results[self._index].name
+        if self._results.results[self._index].media_type == "movie":
+            name = self._results.results[self._index].title
+        elif self._results.results[self._index].media_type == "tv":
+            name = self._results.results[self._index].name
         else:
             name = "???"
         self.embed.title = f"Request for {name} Sent! 🎉"
         self.disable_all_items()
         self.stop()
         await interaction.followup.send(
-            embed=self.embed, view=self,
+            embed=self.embed,
+            view=self,
         )
 
     @discord.ui.button(style=discord.ButtonStyle.danger, label="Cancel")
@@ -169,15 +180,24 @@ class SearchView(discord.ui.View):
         await interaction.response.edit_message(embed=self.embed, view=self)
 
     async def _update_buttons(self) -> None:
+        """
+        Done for embed time check
+        """
         # Previous button
-        self.children[0].disabled = self._index <= 0
+        self.children[0].disabled = self._results.page == 1 and self._index <= 0
         # Next button
-        self.children[1].disabled = self._index >= self._result_length - 1
+        self.children[1].disabled = (
+            self._results.total_pages == self._results.page
+            and self._index * self._results.page >= self._result_length - 1
+        )
 
     async def _edit_embed(self) -> None:
         await self._update_buttons()
         self.embed.clear_fields()
-        result = self._results[self._index]
+        result: OverseerrTypes.PersonResult | OverseerrTypes.MovieResult | OverseerrTypes.TvResult = self._results.results[
+            self._index
+        ]
+        result_number = (self._index + 1) + (self._results.page - 1) * 20
         print(result)
         if result.media_type == "movie":
             self.embed.title = result.title
@@ -187,21 +207,20 @@ class SearchView(discord.ui.View):
             if result.poster_path:
                 self.embed.set_thumbnail(url=self._poster_base + result["posterPath"])
             self.embed.add_field(
-                name="Released", value=result["releaseDate"], inline=True
+                name="Released", value=result.release_date, inline=True
             )
             self.embed.add_field(
-                name="Language", value=result["originalLanguage"], inline=True
+                name="Language", value=result.original_language, inline=True
             )
             self.embed.add_field(
-                name="Popularity", value=f'{int(result["popularity"])}%', inline=False
+                name="Popularity", value=f"{int(result.popularity)}%", inline=False
             )
             self.embed.add_field(
-                name="Vote Average", value=f"{result['voteAverage']:.1f}", inline=True
+                name="Vote Average", value=f"{result.vote_average}", inline=True
             )
             self.embed.add_field(
-                name="Vote Count", value=result["voteCount"], inline=True
+                name="Vote Count", value=result.vote_count, inline=True
             )
-            self.embed.set_footer(text=f"{result['title']} | ID: {result['id']}")
         elif result.media_type == "tv":
             self.embed.description = result["overview"]
             self.embed.add_field(
@@ -211,6 +230,9 @@ class SearchView(discord.ui.View):
             self.embed.title = result.name
             self.embed.set_thumbnail(url=self._backdrop_base + result.profile_path)
             self.embed.add_field(name="test", value="TODO", inline=False)
+        self.embed.set_footer(
+            text=f"Result {result_number} out of {self._result_length}\n\n{result.title} | ID: {result['id']}"
+        )
 
 
 def setup(bot: commands.Bot):
